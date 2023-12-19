4226ffb3f26b2cc25f67a19e8ac34ac3e044a4a0
sandeepjacob-20 <sandeepjacob1@gmail.com>
2023-12-20 00:14:41 +0530

    bug fix #3

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ def foodIdentifierEngine(file):
 
 
 
-st.markdown("<h1 style='text-align:center; font-family: garamond; font-size: 10vh'>Let Him Cook</h1>", unsafe_allow_html=True)   
+st.markdown("<h1 style='text-align:center; font-family: garamond; font-size: 5vw'>Let Him Cook</h1>", unsafe_allow_html=True)   
 st.divider()
 st.subheader("Upload an image of the dish you want to cook")
 file = st.file_uploader("", type=["jpg", "jpeg", "png"])
@@ -25,20 +25,21 @@ if file is None:
     st.caption("Please upload the image in .jpg, .jpeg or .png format")
 
 if file is not None:
-    placeholder = st.empty()
-    with placeholder.container():
-        st.caption("Please wait while we analyze the image and generate the recipe...")
-        try:
+    try:
+        placeholder = st.empty()
+        with placeholder.container():
+            st.caption("Please wait while we analyze the image and generate the recipe...")
+            
             response = foodIdentifierEngine(file)
         
-            placeholder.empty()
-            st.write(response["recipe"])
-            st.divider()
-            st.caption("This recipe was brought to you in "+str(response["prompt_generation_time"])+" seconds")
-            if (st.download_button("Download this recipe",data=response["recipe"],file_name="recipe.txt")):
-                st.snow()
-                st.success("File downloaded", icon='✅')
-        except:
-            print("Some unexpected error occured, Please try again!!")
+        placeholder.empty()
+        st.write(response["recipe"])
+        st.divider()
+        st.caption("This recipe was brought to you in "+str(response["prompt_generation_time"])+" seconds")
+        if (st.download_button("Download this recipe",data=response["recipe"],file_name="recipe.txt")):
+            st.snow()
+            st.success("File downloaded", icon='✅')
+    except:
+        st.write("Some unexpected error occured, Please try again!!")
 
     
