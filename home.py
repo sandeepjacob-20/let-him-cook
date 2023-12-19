d8cf1be5099c4a4a0b4a01ebf9838f7c3beaa3dd
sandeepjacob-20 <sandeepjacob1@gmail.com>
2023-12-19 22:20:28 +0530

    added data caching for faster response

--- home.py
+++ home.py
@@ -3,12 +3,8 @@ from PIL import Image
 import os
 import generative_engine
 
-
-st.markdown("<h1 style='text-align:center; font-family: garamond;'>Let Him Cook</h1>", unsafe_allow_html=True)   
-file = st.file_uploader("Upload file", type=["jpg", "jpeg", "png"])
-if file is None:
-    st.caption("Please upload an image of the dish you want to cook")
-if file is not None:
+@st.cache_data
+def processor(file):
     with open(os.path.join("temp",file.name),"wb") as f:
         f.write(file.getbuffer())
 
@@ -20,7 +16,20 @@ if file is not None:
     st.caption("This receipie was brought to you in "+str(response["prompt_generation_time"])+" seconds")
     os.remove('./temp/'+file.name)
 
-    # if (st.download_button("Download this receipie",data=response["receipe"],file_name="receipie.txt")):
-    #     st.success("File downloaded", icon='✅')
+    return response
+
+
+
+st.markdown("<h1 style='text-align:center; font-family: garamond; font-size: 4vw'>Let Him Cook</h1>", unsafe_allow_html=True)   
+st.divider()
+st.subheader("Upload an image of the dish you want to cook")
+file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+if file is None:
+    st.caption("Please upload the image in .jpg, .jpeg or .png format")
+if file is not None:
+    response = processor(file)
+    if (st.download_button("Download this receipie",data=response["receipe"],file_name="receipie.txt")):
+        st.snow()
+        st.success("File downloaded", icon='✅')
 
     
